f3b02f7aaac989152b4721af6f90b9381ea73c7f
Matheus <MatheusMiguelds@gmail.com>
2024-04-05 10:20:12 -0300

    Adicionando conteúdo sobre decisões

--- modulo-um/aula05/decisoes.php
+++ modulo-um/aula05/decisoes.php
@@ -1,11 +1,16 @@
 <?php
 
 $idade = 17;
+$acompanhamento = 1;
 
-echo "Você só pode entrar se tiver mais que 18 anos".PHP_EOL;
+echo "Você só pode entrar se tiver mais que 18 anos ou a partir de 16 anos acompanhado".PHP_EOL;
 
 if ($idade >= 18) {
-    echo "Você tem $idade anos. Pode entrar";
-} else {
-    echo "Você tem $idade anos, Não pode entrar";
+    echo "Você tem $idade anos. Pode entrar".PHP_EOL;
+}else if ($idade >= 16 && $acompanhamento > 0) {
+    echo "Você tem $idade anos, mas veio acompanhado(a). Pode entrar".PHP_EOL;
+}else {
+    echo "Você tem $idade anos, Não pode entrar".PHP_EOL;
 }
+
+echo "Adeus!";
